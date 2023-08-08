baabf85f9fbead0bce10986bdcf585fb640acffd
AndersonTM30 <andersondev30@gmail.com>
2023-08-07 22:25:12 -0300

    Frist Commit

---
+++ funcoes.exs
@@ -0,0 +1,13 @@
+defmodule MeuModulo do
+    import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
+    def ola_mundo do
+        inspect("Olá Mundo!")
+    end
+
+    def inspect(parametro) do
+        puts("Começando a inspeção")
+        puts(parametro)
+        puts("Terminando a inspeção")
+    end
+end
