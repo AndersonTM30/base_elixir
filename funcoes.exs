da97870e565d704f7e63e078979d1359bb3c76f1
AndersonTM30 <andersondev30@gmail.com>
2023-08-09 21:39:26 -0300

    feat: conceito inicial de require macros

--- funcoes.exs
+++ funcoes.exs
@@ -3,10 +3,15 @@ defmodule MeuModulo do
     import Kernel, except: [inspect: 1]
 
     alias MeuModulo.Math, as: MyMath
+    require Integer
 
     def ola_mundo do
         # inspect("Olá Mundo!")
         inspect(MyMath.soma(2, 2))
+    end
+
+    def exibe_se_eh_par(numero) do
+        puts("O número #{numero} é par? #{Integer.is_even(numero)}")
     end
 
     def inspect(parametro) do
