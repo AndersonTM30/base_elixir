d998ed15699b3d24f6822cd5e9fa6bf0c404a2cc
AndersonTM30 <andersondev30@gmail.com>
2023-08-12 21:46:05 -0300

    feat: finalizando os primeiros exercícios sobre operadores, concatenação de string, condicionais, duplicação de listas, mapas, tuplas e desestruturação

--- exercise/exercise01.exs
+++ exercise/exercise01.exs
@@ -16,4 +16,15 @@ defmodule Exercicio01 do
     def listaDuplicada(lista) do
         lista ++ lista
     end
+
+    def mediaNotasAluno(grades) do
+        total = Enum.reduce(grades, 0, fn {_student, grade}, acc -> acc + grade end)
+        length = Map.size(grades)
+        if length > 0, do: total / length, else: 0
+    end
+
+    def apresentarPessoa(tupla) do
+        { nome, idade, profissao } = tupla
+        IO.puts("Nome: #{nome} | Idade: #{idade} | Profissão: #{profissao}")
+    end
 end
